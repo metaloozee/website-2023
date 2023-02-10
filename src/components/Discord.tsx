67b8d4fa3588b097da602d6c00efc529837a3369
Ayan <ayanparkar123@gmail.com>
2023-02-10 21:47:59 +0530

    fix (Discord.tsx): Discord card

--- src/components/Discord.tsx
+++ src/components/Discord.tsx
@@ -7,6 +7,29 @@ const Discord: React.FC<{ status: string, activities: object[] }> = ({ status, a
     return (
         <>
             <div className={clsx(
+                'group flex h-full w-full flex-col gap-10 justify-between overflow-hidden rounded-2xl text-white',
+                {
+                    online: 'bg-green-500 text-green-50',
+                    idle: 'bg-orange-400 text-orange-50 ',
+                    dnd: 'bg-red-500 text-red-50 ',
+					offline: 'bg-indigo-500 text-white/90 ',
+                }[status]
+            )}>
+                <span className="h-full flex flex-col justify-between px-6 pt-6">
+                    <span className="flex justify-between">
+                        <SiDiscord className="text-3xl" />
+                    </span>
+
+                    <span className="space-y-0.5 pb-6">
+                        <span className="block font-bold">{status}</span>
+                        <span className="block font-semibold text-sm">metalooze#0499</span>
+                        <span className="block text-xs">
+                            {activities.map((a: any) => (a.name == "Code") ? a.details : "")}
+                        </span>
+                    </span>
+                </span>
+            </div>
+            {/* <div className={clsx(
                 'rounded-xl p-10 flex justify-center h-full',
                 {
                     online: 'bg-green-500 text-green-50 ',
@@ -25,7 +48,7 @@ const Discord: React.FC<{ status: string, activities: object[] }> = ({ status, a
                         </h1>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
