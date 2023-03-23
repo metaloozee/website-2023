a0e88ae2ba35caa9bfcfe0abd3d79ed14b4f432a
Ayan <ayanparkar123@gmail.com>
2023-03-23 20:32:20 +0530

    feat: removed example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { guestbookRouter } from "./routers/guestbook";
 
 /**
@@ -8,7 +7,6 @@ import { guestbookRouter } from "./routers/guestbook";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   guestbook: guestbookRouter
 });
 
